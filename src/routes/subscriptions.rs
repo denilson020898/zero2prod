f74c4e8b69ee50f48c4799130adc6d86391cbaa5
denilson <denilson020898@gmail.com>
2023-04-28 07:35:14 +0700

    request id abstraction

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,6 @@ use chrono::Utc;
 
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
-use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -14,7 +13,6 @@ pub struct FormData {
     name = "Adding a new subscriber",
     skip(form, pool),
     fields(
-        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name
     )
