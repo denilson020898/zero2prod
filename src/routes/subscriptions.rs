08563a87eff29d4e98060b7cf6c838e58e51e397
denilson <denilson020898@gmail.com>
2023-05-11 19:31:50 +0700

    add basic auth for newsletter endpoint

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -57,7 +57,7 @@ impl std::error::Error for StoreTokenError {
     }
 }
 
-fn error_chain_fmt(
+pub fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
@@ -193,7 +193,7 @@ pub async fn send_confirmation_email(
     );
 
     email_client
-        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .send_email(&new_subscriber.email, "Welcome!", &html_body, &plain_body)
         .await
 }
 
