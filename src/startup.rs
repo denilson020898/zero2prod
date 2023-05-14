11b59dffc2466077d4946282e979cccca0ed21a0
denilson <denilson020898@gmail.com>
2023-05-14 16:25:56 +0700

    add redis middleware dependency

--- src/startup.rs
+++ src/startup.rs
@@ -2,9 +2,14 @@ use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
 use crate::routes::{home, login, login_form};
+use actix_session::storage::RedisSessionStore;
+use actix_session::SessionMiddleware;
+use actix_web::cookie::Key;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use secrecy::Secret;
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -24,7 +29,7 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(configuration: Settings) -> Result<Application, std::io::Error> {
+    pub async fn build(configuration: Settings) -> Result<Application, anyhow::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
 
         let sender_email = configuration
@@ -51,7 +56,10 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
-        )?;
+            configuration.application.hmac_secret,
+            configuration.redis_uri,
+        )
+        .await?;
 
         Ok(Self { port, server })
     }
@@ -65,18 +73,30 @@ impl Application {
     }
 }
 
-pub fn run(
+pub async fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
-) -> Result<Server, std::io::Error> {
+    hmac_secret: Secret<String>,
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     // arc smart pointer
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+    let message_store = CookieMessageStore::builder(secret_key.clone()).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
+            .wrap(SessionMiddleware::new(
+                redis_store.clone(),
+                secret_key.clone(),
+            ))
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
@@ -89,8 +109,12 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
     Ok(server)
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -8,11 +8,12 @@ async fn an_error_flash_message_is_set_on_failure() {
         "password": "random-password",
     });
     let response = app.post_login(&login_body).await;
+
     assert_is_redirect_to(&response, "/login");
 
     let html_page = app.get_login_html().await;
     assert!(html_page.contains("<p><i>Authentication failed</i></p>"));
 
-    // let html_page = app.get_login_html().await;
-    // assert!(!html_page.contains("Authentication failed"));
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains("<p><i>Authentication failed</i></p>"));
 }
