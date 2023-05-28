449fd4eea9bd14217a8f5229bcc583e15461596e
denilson <denilson020898@gmail.com>
2023-05-28 19:39:44 +0700

    not sure

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -3,8 +3,21 @@ use std::time::Duration;
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
+use fake::faker::internet::en::SafeEmail;
+use fake::faker::name::en::Name;
+use fake::Fake;
+use wiremock::MockBuilder;
+
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
-    let body = "name=le%20guin&email=ursula_le_guin%40yopmail.com";
+    let name: String = Name().fake();
+    let email: String = SafeEmail().fake();
+
+    let body = serde_urlencoded::to_string(&serde_json::json!({
+        "name": name,
+        "email": email,
+    }))
+    .unwrap();
+
     let _mock_guard = Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
@@ -62,6 +75,7 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     // Act - Part 2 - Follow the redirect
     let html_page = app.get_publish_newsletter_html().await;
     assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+    app.dispatch_all_pending_emails().await;
     // Mock verifies on Drop that we haven't sent the newsletter email
 }
 
@@ -117,7 +131,9 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
     // Act - Part 2 - Follow the redirect
     let html_page = app.get_publish_newsletter_html().await;
     assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
     // Mock verifies on Drop that we have sent the newsletter email
+    app.dispatch_all_pending_emails().await;
 }
 
 // #[tokio::test]
@@ -235,6 +251,9 @@ async fn newsletter_creation_is_idempotent() {
 
     let html_page = app.get_publish_newsletter_html().await;
     assert!(dbg!(html_page).contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+
+    app.dispatch_all_pending_emails().await;
 }
 
 #[tokio::test]
@@ -260,5 +279,55 @@ async fn concurrenet_form_submission_is_handled_gracefully() {
     let response2 = app.post_publish_newsletter(&newsletter_request_body);
     let (response1, response2) = tokio::join!(response1, response2);
     assert_eq!(response1.status(), response2.status());
-    assert_eq!(response1.text().await.unwrap(), response2.text().await.unwrap());
+    assert_eq!(
+        response1.text().await.unwrap(),
+        response2.text().await.unwrap()
+    );
+
+    app.dispatch_all_pending_emails().await;
+}
+
+fn when_sending_an_email() -> MockBuilder {
+    Mock::given(path("/email")).and(method("POST"))
+}
+
+#[tokio::test]
+async fn transient_errors_do_not_cause_duplicate_deliveries_on_retries() {
+    let app = spawn_app().await;
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string(),
+    });
+
+    create_confirmed_subscriber(&app).await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(200))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(500))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 500);
+
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .named("Delivery retry")
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 303);
 }
